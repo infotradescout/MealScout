8c224344c4d31ed6eecc0c1e78c8c7b3e7293efe
flavo <flavo@users.noreply.github.com>
2026-04-26 21:31:35 -0500

    Remove production build ID overlay from UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -623,23 +623,6 @@ function App() {
             <Router />
             <Navigation scope="global" />
           </AdminInlineCopyProvider>
-          {/* Build canary - shows deployment timestamp */}
-          {import.meta.env.PROD && (
-            <div
-              style={{
-                position: "fixed",
-                bottom: 4,
-                right: 6,
-                fontSize: 10,
-                opacity: 0.4,
-                zIndex: 99999,
-                pointerEvents: "none",
-                fontFamily: "monospace",
-              }}
-            >
-              {typeof __BUILD_ID__ !== "undefined" ? __BUILD_ID__ : "dev"}
-            </div>
-          )}
         </div>
       </TooltipProvider>
     </QueryClientProvider>
