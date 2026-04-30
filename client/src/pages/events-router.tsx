ecd09b3cb5a0009b4d46b178221819236810c56a
infotradescout <info.tradescout@gmail.com>
2026-04-30 17:11:31 -0500

    Route public events to event-first hub

--- client/src/pages/events-router.tsx
+++ client/src/pages/events-router.tsx
@@ -1,5 +1,18 @@
+import { useMemo } from "react";
+import { useLocation } from "wouter";
 import EventsPage from "@/pages/events";
+import EventsPublicHub from "@/pages/events-public-hub";
 
 export default function EventsRouter() {
-  return <EventsPage />;
+  const [location] = useLocation();
+  const mode = useMemo(() => {
+    const query = location.includes("?") ? location.split("?")[1] : "";
+    return new URLSearchParams(query).get("mode");
+  }, [location]);
+
+  if (mode === "manage") {
+    return <EventsPage />;
+  }
+
+  return <EventsPublicHub />;
 }
