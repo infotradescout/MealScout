82752322337dcc1446c5a3541088cd5e8ae84e94
infotradescout <info.tradescout@gmail.com>
2026-05-01 15:07:17 -0500

    Remove unused media route import

--- server/routes/mediaRoutes.ts
+++ server/routes/mediaRoutes.ts
@@ -1,5 +1,5 @@
 import type { Express } from "express";
-import { and, desc, eq, sql } from "drizzle-orm";
+import { and, desc, eq } from "drizzle-orm";
 
 import { db } from "../db";
 import { storage } from "../storage";
